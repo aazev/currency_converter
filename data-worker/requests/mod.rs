dacc7e453fe06cfba28e1112e59318811518f02a
André Azevedo <andre.azev@gmail.com>
2023-03-06 00:42:14 -0300

    Fixed untracked files;

--- data-worker/requests/mod.rs
+++ data-worker/requests/mod.rs
@@ -1,27 +1,27 @@
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum RequestType {
+pub enum RabbitMQRequestType {
     Quotation,
     Fluctuation,
     Symbols,
 }
 
-impl FromStr for RequestType {
+impl FromStr for RabbitMQRequestType {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "Quotation" => Ok(RequestType::Quotation),
-            "Fluctuation" => Ok(RequestType::Fluctuation),
-            "Symbols" => Ok(RequestType::Symbols),
+            "Quotation" => Ok(RabbitMQRequestType::Quotation),
+            "Fluctuation" => Ok(RabbitMQRequestType::Fluctuation),
+            "Symbols" => Ok(RabbitMQRequestType::Symbols),
             _ => Err(format!("{} is not a valid request type", s)),
         }
     }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Request {
+pub struct RabbitMQRequest {
     date_start: NaiveDate,
     date_end: NaiveDate,
     base_symbol: String,
-    request_type: RequestType,
+    request_type: RabbitMQRequestType,
 }
